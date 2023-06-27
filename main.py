61778ae90bf106ccf2d68b11bfc2a99dd6cab066
HENRIQUE COLE FERNANDES <henrique_c_fernandes@estudante.sesisenai.org.br>
2023-06-27 20:56:25 -0300

    feat: generating the wordcloud's image

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 comments = pd.read_csv("comentarios.csv")
 
@@ -20,4 +22,15 @@ test, train, sentimental_test, sentimental_train = train_test_split(
 lr = LogisticRegression()
 lr.fit(train, sentimental_train)
 accuracy = lr.score(test, sentimental_test)
-print("Accuracy: ", accuracy)
+
+text = " ".join([comment for comment in comments.text])
+
+words_clouds = WordCloud(width=1024, height=768, collocations=False,
+                         background_color="#000").generate(text)
+
+# # Open a plot of the generated image.
+# plt.imshow(words_clouds)
+# plt.show()
+
+# Save the words in a file
+words_clouds.to_file("wordcloud.png")
